b2044092fb950d91155f7110db166eb277121d34
minerva-b <64626133+minerva-b@users.noreply.github.com>
2020-09-19 11:11:21 -0700

    Bar chart and bubble chart up; need to work on demographics table

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
-// Use the D3 library to read in samples.json:
-// ------------------------------------------
+// 1. Use the D3 library to read in samples.json:
+// ----------------------------------------------
 var names  = function() {
     d3.json('../samples.json').then(function(data) {
             var names = data.names
@@ -10,11 +10,6 @@ var names  = function() {
     })   
 };
 
-// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual:
-    // Use sample_values as the values for the bar chart
-    // Use otu_ids as the labels for the bar chart
-    // Use otu_labels as the hovertext for the chart
-// ------------------------------------------
 d3.json('../samples.json').then(function(data) {
     names()
     var s_values = data.samples[0].sample_values
@@ -26,6 +21,11 @@ d3.json('../samples.json').then(function(data) {
     demographics()
 });
 
+// 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual:
+    // Use sample_values as the values for the bar chart
+    // Use otu_ids as the labels for the bar chart
+    // Use otu_labels as the hovertext for the chart
+// --------------------------------------------------
 var barPlot = function(ids, sample_values, hovText) {
     var trace = {
         type: 'bar',
@@ -50,3 +50,38 @@ var barPlot = function(ids, sample_values, hovText) {
 
     Plotly.newPlot('bar', data, layout)
 };
+
+// 3. Create a bubble chart that displays each sample:
+    // Use otu_ids for the x values
+    // Use sample_values for the y values
+    // Use sample_values for the marker size
+    // Use otu_ids for the marker colors
+    // Use otu_labels for the text values
+// ---------------------------------------------------
+var bubblePlot = function(ids, sample_values, hovText) {
+    var trace = {
+        x: ids,
+        y: sample_values,
+        mode: 'markers',
+        marker: {
+            size: sample_values,
+            color: ids
+        },
+        text: hovText
+    };
+
+    var data = [trace];
+
+    var layout = {
+        title: "Bubble chart of OTU IDs",
+        xaxis: {
+            title: "OTU ID"
+        }
+    };
+
+    Plotly.newPlot('bubble', data, layout)
+};
+
+// 4. Display the sample metadata, i.e., an individual's demographic information:
+// 5. Display each key-value pair from the metadata JSON object somewhere on the page:
+// --------------------------------------------------------------------------------
